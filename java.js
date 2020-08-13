6ad5dd4f893fb027b5f35684fc52ba307bcd3a53
scaryjoshie <67752308+scaryjoshie@users.noreply.github.com>
2020-08-13 14:36:43 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+var simple=function(){
+   var textMultiple = {
+         popup_text:"This page will display private information, are you sure you want to proceed? (page will close automatically if you press cancel)",
+    };
+   return textMultiple;
+}
+
+var ip_address = HttpRequest.ServerVariables["REMOTE_ADDR"];
